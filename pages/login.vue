5bb98289dd338633c1ff662267ba102c1dddd2d9
julesdevops <julesbourdais7198@gmail.com>
2021-01-29 22:18:08 +0100

    feat(ui): Add a 'login view'
    Add a place where the user will enter their username so they can start
    messaging²

---
+++ pages/login.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row>
+    <v-col md="3" class="mx-auto">
+      <v-card class="text-center">
+        <v-card-title class="justify-center my-3">
+          Welcome to Message Hut
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              counter="20"
+              label="Enter your name"
+              class="my-4"
+              v-model="username"
+            >
+            </v-text-field>
+            <v-btn color="primary" @click="handleStartMessagingButtonClick"
+              >Start messaging
+              <v-icon right>mdi-arrow-right-bold</v-icon></v-btn
+            >
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+    }
+  },
+  methods: {
+    handleStartMessagingButtonClick() {},
+  },
+}
+</script>
