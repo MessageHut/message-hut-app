6575ce423c15b45e9e79553b7dc814439a63902c
julesdevops <julesbourdais7198@gmail.com>
2021-01-29 22:22:57 +0100

    feat(ui): define a new 'minimal' layout and use it on the login page

---
+++ layouts/minimal.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer :absolute="!fixed" app class="justify-center">
+      <span>&copy; Message Hut {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
