fb090828af3b85a744eab948ef27d7006540dea7
julesdevops <julesbourdais7198@gmail.com>
2021-04-11 17:14:52 +0200

    chore: get db-migrate to work, create base table

--- migrations/20210410195513-create-messages-table.js
+++ migrations/20210410195513-create-messages-table.js
@@ -14,12 +14,20 @@ exports.setup = function(options, seedLink) {
   seed = seedLink;
 };
 
-exports.up = function(db) {
-  return null;
+exports.up = async function(db) {
+  await db.createTable('messages', {
+    columns: {
+      id: { type: 'bigint', unsigned: true, primaryKey: true, autoIncrement: true },
+      content: { type: 'text', notNull: true },
+      created_at: { type: 'datetime', notNull: true },
+      updated_at: { type: 'datetime', notNull: true }
+    },
+    ifNotExists: true
+  })
 };
 
-exports.down = function(db) {
-  return null;
+exports.down = async function(db) {
+  await db.dropTable('messages', { ifExists: true })
 };
 
 exports._meta = {
--- migrations/20210410195525-create-conversations-table.js
+++ migrations/20210410195525-create-conversations-table.js
@@ -14,12 +14,20 @@ exports.setup = function(options, seedLink) {
   seed = seedLink;
 };
 
-exports.up = function(db) {
-  return null;
+exports.up = async function(db) {
+  await db.createTable('conversations', {
+    columns: {
+      id: { type: 'bigint', unsigned: true, primaryKey: true, autoIncrement: true },
+      name: { type: 'string', length: 50, notNull: true },
+      created_at: { type: 'datetime', notNull: true },
+      updated_at: { type: 'datetime', notNull: true }
+    },
+    ifNotExists: true
+  })
 };
 
-exports.down = function(db) {
-  return null;
+exports.down = async function(db) {
+  await db.dropTable('conversations', { ifExists: true })
 };
 
 exports._meta = {
