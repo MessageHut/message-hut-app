c3aabd107ab2cd92bad4e8599fa1d8a70fa3963e
julesdevops <julesbourdais7198@gmail.com>
2021-04-11 17:38:37 +0200

    chore: create foreign keys & many2many table

--- migrations/20210410195526-create-messages-table.js
+++ migrations/20210410195526-create-messages-table.js
@@ -19,6 +19,36 @@ exports.up = async function(db) {
     columns: {
       id: { type: 'bigint', unsigned: true, primaryKey: true, autoIncrement: true },
       content: { type: 'text', notNull: true },
+      user_id: {
+        type: 'bigint', 
+        unsigned: true, 
+        notNull: true,
+        foreignKey: {
+          name: 'messages_user_id_fk',
+          table: 'users',
+          rules: {
+            onDelete: 'CASCADE'
+          },
+          mapping: {
+            user_id: 'id'
+          }
+        }
+      },
+      conversation_id: {
+        type: 'bigint', 
+        unsigned: true, 
+        notNull: true,
+        foreignKey: {
+          name: 'messages_conversation_id_fk',
+          table: 'conversations',
+          rules: {
+            onDelete: 'CASCADE'
+          },
+          mapping: {
+            conversation_id: 'id'
+          }
+        }
+      },
       created_at: { type: 'datetime', notNull: true },
       updated_at: { type: 'datetime', notNull: true }
     },
